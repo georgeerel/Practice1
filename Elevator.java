73bcbba269f81197b895f6398f67bac619a3f553
georgeerel <georgeerel1@gmail.com>
2025-06-26 19:57:56 +0300

    Mater Branch

---
+++ Elevator.java
@@ -0,0 +1,22 @@
+import java.security.PublicKey;
+
+public class Elevator {
+    public boolean isAtTop() {
+        return false;
+    }
+    public boolean isAtBottom(){
+        return false;
+    }
+    public void goToTop(){
+
+    }
+    public void goToBottom(){
+
+    }
+    public boolean isAtTarget(){
+        return false;
+    }
+
+
+
+}
